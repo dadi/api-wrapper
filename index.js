ff2b31044a31f11f76b308234866d727ab3ca4af
Eduardo Bouças <mail@eduardoboucas.com>
2016-04-26 21:06:09 +0100

    Use dynamic filenames for token wallets

--- index.js
+++ index.js
@@ -18,9 +18,28 @@ var DadiAPI = function (options) {
     credentials: options.credentials,
     wallet: 'file',
     walletOptions: {
-      path: __dirname + '/token.json'
+      path: __dirname + '/.wallet/token.' + this._slugify(options.uri + options.port) + '.' + this._slugify(options.credentials.clientId) + '.json'
     }
   };
+};
+
+/**
+ * Creates a URL/filename friendly version (slug) of any object that implements `toString()`
+ *
+ * @param {Object} input - object to be slugified
+ * @return String
+ * @api private
+ */
+DadiAPI.prototype._slugify = function (input) {
+  return input.toString()
+    .toLowerCase()
+    .replace(/[\?\#][\s\S]*$/g, '')
+    .replace(/\/+/g, '-')
+    .replace(/\s+/g, '')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
 };
 
 /**
